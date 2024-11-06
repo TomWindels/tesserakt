562b0fb27471a4acb936d4d467d05ada88a3ab20
Tom Windels <tom.windels@outlook.com>
2024-11-06 13:56:08 +0100

    Disabled expect/actual warnings for benchmark & component conventions

--- buildSrc/src/main/kotlin/component-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/component-conventions.gradle.kts
@@ -13,4 +13,15 @@ kotlin {
     }
     mingwX64()
     linuxX64()
+
+    // silencing expect/actual warnings
+    targets.all {
+        compilations.all {
+            compileTaskProvider.configure {
+                compilerOptions {
+                    freeCompilerArgs.add("-Xexpect-actual-classes")
+                }
+            }
+        }
+    }
 }
