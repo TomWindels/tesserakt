a5a13a976ab32dfd79787de530f8adac45fb27c0
Tom Windels <tom.windels@ugent.be>
2025-02-04 16:45:46 +0100

    Added dynamic formatting output flattening
    Added a new flatten strategy type, which can decide what token groups to flatten in structure for the output

--- serialization/n3/src/commonMain/kotlin/dev/tesserakt/rdf/serialization/Formatter.kt
+++ serialization/n3/src/commonMain/kotlin/dev/tesserakt/rdf/serialization/Formatter.kt
@@ -1,5 +1,6 @@
 package dev.tesserakt.rdf.serialization
 
+import dev.tesserakt.util.addFront
 import kotlin.jvm.JvmInline
 
 
@@ -28,6 +29,10 @@ data class PrettyFormatter(
      *  a set of spaces or tabs
      */
     val indent: Indent = SimpleIndent("    "),
+    /**
+     * The strategy used to flatten block structures
+     */
+    val flattenStrategy: FlattenStrategy = LengthBasedFlattening(64),
 ): Formatter() {
 
     /**
@@ -41,18 +46,54 @@ data class PrettyFormatter(
         var next: N3Token? = if (iterator.hasNext()) iterator.next() else null
             private set
 
+        private val buf = mutableListOf<N3Token>()
+
+        /**
+         * A list of upcoming tokens, starting from [next], containing all following tokens matching those peeked in
+         *  [peekUntil] (including the one not satisfying the predicate)
+         *
+         * IMPORTANT: this can include more non-matching tokens, if [peekUntil] is used more frequently than [advance]
+         */
+        val upcoming: List<N3Token>
+            // if next == null, the buf should be guaranteed empty
+            get() = next?.let { next -> buf.addFront(next) } ?: emptyList()
+
         /**
          * Advances [current] and [next] read tokens, returning `true` if [current] has been updated, or false if the
          *  end has been reached
          */
         fun advance(): Boolean {
             current = next ?: return false
-            next = if (iterator.hasNext()) iterator.next() else null
+            next = when {
+                buf.isNotEmpty() -> buf.removeFirst()
+                iterator.hasNext() -> iterator.next()
+                else -> null
+            }
             return true
         }
 
+        /**
+         * Consumes tokens for "lookahead" use until [predicate] returns `false` or no more tokens are available. These
+         *  tokens are stored for future use through [current], [next] and [advance], including the one resulting
+         *  in `false`.
+         */
+        inline fun peekUntil(predicate: (N3Token) -> Boolean) {
+            val next = next ?: /* end was reached */ return
+            if (!predicate(next)) {
+                // first one already doesn't match, not consuming any further
+                return
+            }
+            while (iterator.hasNext()) {
+                val current = iterator.next()
+                buf.add(current)
+                if (!predicate(current)) {
+                    break
+                }
+            }
+        }
+
         override fun toString(): String {
-            return "TokenBuffer { current: $current, next: $next }"
+            return "TokenBuffer { current: $current, next: $next, buf: $buf }"
         }
 
     }
@@ -64,6 +105,24 @@ data class PrettyFormatter(
     @JvmInline
     value class SimpleIndent(private val pattern: String): Indent {
         override fun create(depth: Int) = pattern.repeat(depth)
+    }
+
+    /**
+     * A class dictating what should and should not be flattened in representation for inner structural blocks of formatted outputs.
+     *  Note: blocks containing blocks themselves are never subject to this check
+     */
+    sealed class FlattenStrategy {
+        internal abstract fun shouldBeFlattened(content: List<N3Token>): Boolean
+    }
+
+    data object NoFlattening: FlattenStrategy() {
+        override fun shouldBeFlattened(content: List<N3Token>) = false
+    }
+
+    data class LengthBasedFlattening(private val maxLength: Int): FlattenStrategy() {
+        override fun shouldBeFlattened(content: List<N3Token>): Boolean {
+            return content.sumOf { it.syntax.length } <= maxLength
+        }
     }
 
     @JvmInline
@@ -113,7 +172,17 @@ data class PrettyFormatter(
 
     }
 
-    private fun processStatements(stack: Stack, buffer: TokenBuffer): Iterator<String> = iterator {
+    override fun format(tokens: Iterator<N3Token>) = processStatements(
+        stack = Stack(),
+        buffer = TokenBuffer(tokens),
+        newlines = true
+    )
+
+    private fun processStatements(
+        stack: Stack,
+        buffer: TokenBuffer,
+        newlines: Boolean,
+    ): Iterator<String> = iterator {
         while (true) {
             if (stack.position == Stack.Position.SUBJECT) {
                 yieldAll(formatToken(stack, buffer))
@@ -157,8 +226,12 @@ data class PrettyFormatter(
                 return@iterator
             }
             // preparing for next iteration
-            yield("\n")
-            yield(indent.create(stack.indent))
+            if (newlines) {
+                yield("\n")
+                yield(indent.create(stack.indent))
+            } else {
+                yield(" ")
+            }
         }
     }
 
@@ -182,27 +255,51 @@ data class PrettyFormatter(
 
     private fun formatStatementsBlock(stack: Stack, buffer: TokenBuffer) = iterator {
         require(buffer.current == N3Token.Structural.StatementsListStart)
+        // scanning ahead for the next tokens, deciding what formatting strategy should be applied
+        // 1: taking all terms part of this block, until we start a new block or end this one
+        buffer.peekUntil { token -> !token.isBlockToken() }
+        val upcoming = buffer.upcoming
+        // 2: scanning the tokens to ensure we don't create a new block inside this one
+        val index = upcoming.indexOfFirst { it.isBlockToken() }
+        val newlines =
+            // the first block token should be the one ending this statement list, so we don't have any child block
+            upcoming[index] == N3Token.Structural.StatementsListEnd &&
+            // if we don't have a child block, we can ask the configured strategy what to do with this block layout
+            !flattenStrategy.shouldBeFlattened(upcoming.take(index))
         // adjusting current statement
-        yield(buffer.current.syntax)
+        yield(buffer.current.syntax) // "{"
         buffer.advance()
         // starting the next statements (yielding relevant tokens) inside a new frame
         stack.startFrame()
-        yield("\n")
-        yield(indent.create(stack.indent))
-        yieldAll(processStatements(stack, buffer))
+        if (newlines) {
+            yield("\n")
+            yield(indent.create(stack.indent))
+        } else {
+            yield(" ")
+        }
+        yieldAll(processStatements(stack, buffer, newlines = newlines))
         if (buffer.current != N3Token.Structural.StatementsListEnd) {
             throw IllegalStateException("Did reach the end of the statements list properly!")
         }
         // terminating this frame
         stack.stopFrame()
-        yield("\n")
-        yield(indent.create(stack.indent))
-        yield(buffer.current.syntax)
+        if (newlines) {
+            yield("\n")
+            yield(indent.create(stack.indent))
+        } else {
+            yield(" ")
+        }
+        yield(buffer.current.syntax) // "}"
         check(buffer.advance())
     }
 
-    override fun format(tokens: Iterator<N3Token>): Iterator<String> {
-        return processStatements(Stack(), TokenBuffer(tokens))
-    }
+    /**
+     * Returns true if this token denotes the start/end of a "block": `[`, `]`, `{` & `}`
+     */
+    private fun N3Token.isBlockToken() =
+        this == N3Token.Structural.BlankStart ||
+        this == N3Token.Structural.BlankEnd ||
+        this == N3Token.Structural.StatementsListStart ||
+        this == N3Token.Structural.StatementsListEnd
 
 }
