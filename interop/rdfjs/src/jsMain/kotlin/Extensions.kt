f3cd6aa8b537e3f932abc42061ba2e53a287f1d6
Tom Windels <tom.windels@outlook.com>
2025-04-10 14:44:06 +0200

    Improved RDFJS interop API
    Changed the JS-facing RDFJS interop API, now supporting different collection types when converting, bundled inside of a single object

--- interop/rdfjs/src/jsMain/kotlin/Extensions.kt
+++
@@ -1,8 +0,0 @@
-import dev.tesserakt.interop.rdfjs.n3.N3Store
-import dev.tesserakt.interop.rdfjs.toN3Store
-
-@OptIn(ExperimentalJsExport::class)
-@JsExport
-fun toRDFJSStore(store: MutableStoreJs): N3Store {
-    return store.unwrap().toN3Store()
-}
