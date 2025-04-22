96ce5111e729e217a700a620d30900c89621d23c
Tom Windels <tom.windels@outlook.com>
2025-04-22 10:25:42 +0200

    Applied Dokka plugin
    Applied the Dokka plugin to the project root, generating HTML documentation for the entire project
    NOTE: `:dokkaHtmlMultiModule` should be executed when generating documentation (`:` is required, see Kotlin/dokka/issues/2954#issuecomment-1694714170)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,20 @@
+import org.jetbrains.dokka.gradle.DokkaTaskPartial
+
 repositories {
     mavenCentral()
+}
+
+plugins {
+    id("org.jetbrains.dokka") version "2.0.0"
+}
+
+subprojects {
+    apply(plugin = "org.jetbrains.dokka")
+
+    // https://github.com/Kotlin/dokka/issues/1727
+    tasks.named<DokkaTaskPartial>("dokkaHtmlPartial") {
+        moduleName.set(this@subprojects.path.substring(1).replace(":", "."))
+    }
 }
 
 buildscript {
