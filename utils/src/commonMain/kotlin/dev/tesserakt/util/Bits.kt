5d35cba76c29ecb65e63156d14462e468c250871
Tom Windels <tom.windels@ugent.be>
2025-09-29 19:21:42 +0200

    BitIterator performance improvement
    Changed BitIterator to inherit from `IntIterator`, allowing the Kotlin compiler to keep primitive `int`s during iteration

--- utils/src/commonMain/kotlin/dev/tesserakt/util/Bits.kt
+++ utils/src/commonMain/kotlin/dev/tesserakt/util/Bits.kt
@@ -3,12 +3,12 @@ package dev.tesserakt.util
 
 fun Int.bitIterator() = BitIterator(this)
 
-class BitIterator(private var bits: Int): Iterator<Int> {
+class BitIterator(private var bits: Int): IntIterator() {
     override fun hasNext(): Boolean {
         return bits != 0
     }
 
-    override fun next(): Int {
+    override fun nextInt(): Int {
         val bit = bits.takeLowestOneBit()
         val index = bit.countTrailingZeroBits()
         bits = bits xor bit
