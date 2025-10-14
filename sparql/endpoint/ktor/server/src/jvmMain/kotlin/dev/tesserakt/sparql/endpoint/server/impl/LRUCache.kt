9f9d11dec9e7f0669f55774fdbfe191ea508dd54
Tom Windels <tom.windels@outlook.com>
2025-10-14 10:34:13 +0200

    Added `onEviction` callback to LRU cache
    Added two new LRUCache tests to ensure correct behaviour w.r.t access order & eviction callback

--- sparql/endpoint/ktor/server/src/jvmMain/kotlin/dev/tesserakt/sparql/endpoint/server/impl/LRUCache.kt
+++ sparql/endpoint/ktor/server/src/jvmMain/kotlin/dev/tesserakt/sparql/endpoint/server/impl/LRUCache.kt
@@ -1,9 +1,16 @@
 package dev.tesserakt.sparql.endpoint.server.impl
 
 // src: https://medium.com/@fasilt/understanding-lru-least-recently-used-cache-in-kotlin-b54c7060e752
-internal class LRUCache<K, V>(private val capacity: Int) : LinkedHashMap<K, V>(capacity, 0.75f, true) {
+internal class LRUCache<K, V>(
+    private val capacity: Int,
+    private val onEviction: (K, V) -> Unit = { _, _ -> },
+) : LinkedHashMap<K, V>(capacity, 0.75f, true) {
 
     override fun removeEldestEntry(eldest: MutableMap.MutableEntry<K, V>?): Boolean {
-        return size > capacity
+        if (size > capacity) {
+            onEviction(eldest!!.key, eldest.value)
+            return true
+        }
+        return false
     }
 }
--- sparql/endpoint/ktor/server/src/jvmTest/kotlin/LRUTest.kt
+++ sparql/endpoint/ktor/server/src/jvmTest/kotlin/LRUTest.kt
@@ -36,4 +36,39 @@ class LRUTest {
         assertEquals("9", cache[9])
     }
 
+    @Test
+    fun outOfOrderAccess() {
+        val cache = LRUCache<Int, String>(3)
+        cache[11] = "11"
+        repeat(10) {
+            // making sure it's recently accessed
+            val access = cache[11]
+            cache[it] = it.toString()
+        }
+        assertContains(cache, 11)
+        assertContains(cache, 8)
+        assertContains(cache, 9)
+        repeat(7) {
+            assert(it !in cache)
+        }
+        assertEquals("11", cache[11])
+        assertEquals("8", cache[8])
+        assertEquals("9", cache[9])
+    }
+
+    @Test
+    fun evictionCallback() {
+        val evicted = mutableSetOf<Int>()
+        val cache = LRUCache<Int, String>(
+            capacity = 3,
+            onEviction = { key, _ -> evicted.add(key) }
+        )
+        repeat(10) {
+            cache[it] = it.toString()
+        }
+        repeat(7) {
+            assertContains(evicted, it)
+        }
+    }
+
 }
