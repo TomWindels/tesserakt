88d93a9ea7e44a57689ddda492f38e41263bece2
Tom Windels <tom.windels@ugent.be>
2025-10-19 16:55:17 +0200

    Changed `Iterable<Quad>.query()` semantics
    Changed how the callback version of querying behaves, now throwing UnsupportedOperationException upon providing a query containing ORDER BY, as the callback method cannot provide correct element ordering

--- sparql/src/commonMain/kotlin/dev/tesserakt/sparql/QueryExtensions.kt
+++ sparql/src/commonMain/kotlin/dev/tesserakt/sparql/QueryExtensions.kt
@@ -12,30 +12,35 @@ import dev.tesserakt.sparql.runtime.query.QueryState
 import dev.tesserakt.sparql.types.SelectQueryStructure
 
 
+/**
+ * Executes the given [query] on this [Iterable] as data source. Results are issued through the provided [callback].
+ * Note that it is possible for results to be removed using [QueryState.ResultChange.Removed] as long as not all data
+ *  has been processed.
+ *
+ * IMPORTANT: the query is not allowed to use `ORDER BY`, as the callback is not aware of result order. Providing such
+ *  a query will throw a [UnsupportedOperationException] instead.
+ */
 fun <RT> Iterable<Quad>.query(
     query: Query<RT>,
     callback: (QueryState.ResultChange<RT>) -> Unit
 ) {
-    return query(query.createState(), callback = callback)
-}
-
-internal inline fun <RT> Iterable<Quad>.query(
-    query: QueryState<RT, *>,
-    callback: (QueryState.ResultChange<RT>) -> Unit
-) {
+    if (query.compiled is SelectQueryStructure && query.compiled.ordering != null) {
+        throw UnsupportedOperationException("The query uses `ORDER BY`, which is not supported through this API! Use regular `query()` methods instead.")
+    }
+    val state = query.createState()
     RuntimeStatistics.reset()
     // setting initial state
-    query.results.forEach {
+    state.results.forEach {
         callback(QueryState.ResultChange.New(it))
     }
     // now incrementally evaluating the input
     val it = iterator()
     while (it.hasNext()) {
-        query
+        state
             .processAndGet(DataAddition(it.next()))
             .forEach { callback(it) }
     }
-    RuntimeStatistics.append(query.debugInformation())
+    RuntimeStatistics.append(state.debugInformation())
 }
 
 fun <RT> Iterable<Quad>.query(query: Query<RT>): List<RT> {
