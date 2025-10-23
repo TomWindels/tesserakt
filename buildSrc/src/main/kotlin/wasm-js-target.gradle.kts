db36a45f49bf680e81d269d904d60ee382b487c9
Tom Windels <tom.windels@outlook.com>
2025-10-23 15:09:00 +0200

    Applied same Yarn lock file logic in the WASM target as the JS target, as it got separated since K2.2

--- buildSrc/src/main/kotlin/wasm-js-target.gradle.kts
+++ buildSrc/src/main/kotlin/wasm-js-target.gradle.kts
@@ -1,9 +1,21 @@
+import org.jetbrains.kotlin.gradle.targets.js.yarn.YarnLockMismatchReport
+import org.jetbrains.kotlin.gradle.targets.wasm.yarn.WasmYarnPlugin
+import org.jetbrains.kotlin.gradle.targets.wasm.yarn.WasmYarnRootExtension
+
 plugins {
     id("base-config")
 }
 
 repositories {
     mavenCentral()
+}
+
+// ensuring the yarn lock file behaves
+// copied from the `js-package` convention plugin, but adapted
+rootProject.plugins.withType(WasmYarnPlugin::class.java) {
+    rootProject.the<WasmYarnRootExtension>().yarnLockMismatchReport = YarnLockMismatchReport.NONE
+    rootProject.the<WasmYarnRootExtension>().reportNewYarnLock = true
+    rootProject.the<WasmYarnRootExtension>().yarnLockAutoReplace = true
 }
 
 kotlin {
