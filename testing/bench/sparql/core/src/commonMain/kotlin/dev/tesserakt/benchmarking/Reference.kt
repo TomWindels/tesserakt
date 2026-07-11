525e1a52e25e5454de2870702a04a10bb562e2fa
Tom Windels <tom.windels@outlook.com>
2026-07-11 19:37:33 +0200

    Remove `:testing:bench:sparql`
    SPARQL benchmarking has been moved to an external project, which loads the engine in as a standalone JAR file. This allows the evaluator to use different versions of the SPARQL engine, allowing comparisons between specific versions to be made without having to adapt the tooling

--- testing/bench/sparql/core/src/commonMain/kotlin/dev/tesserakt/benchmarking/Reference.kt
+++
@@ -1,8 +0,0 @@
-package dev.tesserakt.benchmarking
-
-
-/**
- * A reference implementation. The type is constructed through reflection by providing the class type parameter in the
- *  `Evaluator::reference` method
- */
-abstract class Reference: Evaluator()
--- testing/bench/sparql/src/commonMain/kotlin/dev/tesserakt/benchmarking/RunId.kt
+++
@@ -1,9 +0,0 @@
-package dev.tesserakt.benchmarking
-
-
-data class RunId(
-    val deltaIndex: Int,
-) {
-    fun id() = "Δ${deltaIndex}"
-    override fun toString() = "Δ${deltaIndex}"
-}
