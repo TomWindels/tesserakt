9d07325df2d5669a6eb19a4f8cf4acbe766793b6
Tom Windels <tom.windels@outlook.com>
2026-08-22 16:02:29 +0200

    Initial `BIND` support
    Added a compiled version of `BindingStatement`, `BindingExpression`, which can be invoked from a mapping that is part of the output set
    Changed `BindingsImpl` to support the evaluation of `BindingExpression`s to prevent the necessity of encoding the resulting term in the underlying mapping representation
    Changed `Literal(int)` to use `XSD.integer`, matching Jena's behaviour
    Improved `SelectQueryState`s stats overview
    Added three basic `BIND` tests

--- sparql/runtime/src/commonMain/kotlin/dev/tesserakt/sparql/runtime/query/SelectQueryState.kt
+++ sparql/runtime/src/commonMain/kotlin/dev/tesserakt/sparql/runtime/query/SelectQueryState.kt
@@ -2,8 +2,9 @@ package dev.tesserakt.sparql.runtime.query
 
 import dev.tesserakt.rdf.types.Store
 import dev.tesserakt.sparql.Bindings
+import dev.tesserakt.sparql.QueryStatistics
 import dev.tesserakt.sparql.runtime.evaluation.*
-import dev.tesserakt.sparql.runtime.query.QueryState.ResultChange.Companion.into
+import dev.tesserakt.sparql.runtime.query.QueryState.ResultChange.Companion.materialize
 import dev.tesserakt.sparql.runtime.query.select.OutputState
 import dev.tesserakt.sparql.types.SelectQueryStructure
 import dev.tesserakt.sparql.util.MappedCollection.Companion.mapLazily
@@ -14,10 +15,16 @@ class SelectQueryState(
 ): QueryState<Bindings, SelectQueryStructure>(ast, source) {
 
     private val projectionSet = BindingIdentifierSet(context, ast.bindings)
+    private val finalBindingExpressions = ast.body.bindingStatements.map { statement ->
+        // TODO:
+        //  * skip expressions not visible after projection
+        //  * skip inlined expressions
+        BindingExpression(context, statement)
+    }
 
     private val _results = OutputState(context, ast)
     override val results: Collection<BindingsImpl>
-        get() = _results.mapLazily { it.into(context) }
+        get() = _results.mapLazily { it.materialize(context, finalBindingExpressions) }
 
     init {
         constructInitialState()
@@ -36,10 +43,30 @@ class SelectQueryState(
     }
 
     override fun transformNewBodyResult(change: MappingDelta): ResultChange<Bindings> {
-        val bindings = change.value.retain(projectionSet).into(context)
+        // TODO: make sure we retain those that are required for the binding expression evaluation first,
+        //  retain again after if necessary
+        val bindings = change.value.retain(projectionSet).materialize(context, finalBindingExpressions)
         return when (change) {
             is MappingAddition -> ResultChange.New(bindings)
             is MappingDeletion -> ResultChange.Removed(bindings)
         }
     }
+
+    override fun stats(granularity: QueryStatistics.Granularity): Statistics {
+        val inner = super.stats(granularity)
+        val description = if (
+            granularity isAtLeast QueryStatistics.Granularity.DETAILED &&
+            finalBindingExpressions.isNotEmpty()
+        ) {
+            val expr = finalBindingExpressions.joinToString(separator = "\n")
+            "SELECT\nBindings ${projectionSet.asIntIterable().joinToString()}\n${expr}"
+        } else {
+            "SELECT\nBindings ${projectionSet.asIntIterable().joinToString()}"
+        }
+        return Statistics.DescriptionElement(
+            description = description,
+            inner = inner
+        )
+    }
+
 }
